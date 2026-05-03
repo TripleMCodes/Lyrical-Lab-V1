fe91facd31b7b49cb0283f376cf4723fbd47682f
TripleMCodes <khona6047@gmail.com>
2026-05-03 13:47:01 +0200

    fix: add redirect

--- my_app/src/routes/login/+page.server.js
+++ my_app/src/routes/login/+page.server.js
@@ -57,9 +57,10 @@ export const actions = {
             maxAge: 60 * 60 * 24 * 7
             });
         
-        return { success: true };
+        // return { success: true };
 
         // Success → redirect
+        redirect(303, '/lyrical-lab');
         // throw redirect(303, '/lyrical-lab');
     }
 };
