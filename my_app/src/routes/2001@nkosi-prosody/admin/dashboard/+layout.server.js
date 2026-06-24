571fbf89f346431089e5218ad36ac442d5b9446e
TripleMCodes <khona6047@gmail.com>
2026-06-24 13:49:11 +0200

    add dashboard server layout

---
+++ my_app/src/routes/2001@nkosi-prosody/admin/dashboard/+layout.server.js
@@ -0,0 +1,9 @@
+import { redirect } from '@sveltejs/kit';
+
+export async function load({ cookies }) {
+  const accessToken = cookies.get('access_token');
+
+  if (!accessToken) {
+    throw redirect(303, '/login');
+  }
+}
