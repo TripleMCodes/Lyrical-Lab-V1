d9917488280aade26ce2357de33f381874685fca
TripleMCodes <khona6047@gmail.com>
2026-08-05 13:15:28 +0200

    add new update

--- my_app/src/lib/url_vars/urls_vars.js
+++ my_app/src/lib/url_vars/urls_vars.js
@@ -1,9 +1,9 @@
 export function get_url(){
-    // let url = "https://m-prosody.onrender.com";
-    // if (process.env.NODE_ENV === 'production') {
-    //     url = "https://m-prosody.onrender.com";
-    // }
-    let url = "http://192.168.83.102:8000"
+    let url = "https://m-prosody.onrender.com";
+    if (process.env.NODE_ENV === 'production') {
+        url = "https://m-prosody.onrender.com";
+    }
+    // let url = "http://192.168.0.194:8000"
     return url;
 
 }
